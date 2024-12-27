36b07f81daf477eadd387b66538d449f52abd2f2
nilbhungaliya <nilbhungaliya1204@gmail.com>
2024-12-27 18:49:24 +0530

    this

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -7,8 +7,8 @@ function AppBar() {
     return (
         <div className="border-b flex justify-between px-10 py-4">
             <Link to="/blogs" className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600 hover:from-purple-500 hover:to-pink-500 transition duration-300">
-                        MyBlog
-                    </Link>
+                MyBlog
+            </Link>
             <div>
                 <Link to={`/publish`}>
                     <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New</button>
